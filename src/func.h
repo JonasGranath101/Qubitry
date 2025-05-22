4c1d25dbbd937e1f855fd2b76788d9be44f0d577
Jonas Granath <jonas.general57@gmail.com>
2025-05-22 23:28:50 +0200

    Update func.h

--- src/func.h
+++ src/func.h
@@ -1,13 +1,13 @@
-#ifndef DEFAULT_H
-#define DEFAULT_H
+#ifndef FUNC_H
+#define FUNC_H
 
 // It's good practice to declare functions that will be part of a public API
 // with some form of visibility control if you were building a larger shared library.
 // For cffi, just the declarations are often enough.
 
-int httpSearch(int var_url, int var_host, int var_path, int var_request);
-int xordecrypt(const char *input, char *output, const char *key);
-int data_lambda(int var_a);
-int decrypt_lambda(char var_encrypt);
+int graph(int var_url, int var_host, int var_path, int var_request);
+int mesh(const char *input, char *output, const char *key);
+int index(int var_a);
+int (char var_encrypt);
 
-#endif // DEFAULT_H
+#endif // FUNC_H
