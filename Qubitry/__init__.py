a2e10cc8cc7d926a200fb711f87bbe590ffeacbf
Jonas Granath <jonas.general57@gmail.com>
2025-05-24 19:00:01 +0200

    Delete Qubitry/__init__.py

--- Qubitry/__init__.py
+++
@@ -1,9 +0,0 @@
-# This makes the functions directly available under the package name
-
-### MOD ###
-
-from .core import output, geometry, graph
-
-__all__ = ['output', 'geometry', 'graph']
-
-### MOD ###
